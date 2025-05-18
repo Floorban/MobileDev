7776e93b5c65bff777c1b513ad2bd6ad3bba0660
Eric Li <15696082812@163.com>
2025-05-18 10:15:34 +0200

    hp ui update (maxhp binding)

--- Assets/Scenes/HealthStats.cs
+++ Assets/Scenes/HealthStats.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "HealthStats", menuName = "Scriptable Objects/HealthStats")]
 public class HealthStats : ScriptableObject
 {
-    private float value;
-    public float Value {
-        get => health/maxHealth;
-        set { value = health / maxHealth; }
-    }
-
     [SerializeField] private float health;
     public float maxHealth;
-    public void UpdateHealth() {
-
+    public void UpdateHealth(float healthChange) {
+        health += healthChange;
     }
 }
