9f3453b5e44e46091e71308c73bbfc202fede080
Eric Li <15696082812@163.com>
2025-05-22 21:05:05 +0200

    organize folders

---
+++ Assets/Scripts/Attack/RangedAttack.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class RangedAttack : RangeCheck
+{
+    public RangedWeapon _stats;
+
+}
---
+++ Assets/Scripts/Attack/WeaponStats/Ranged/RangedWeapon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Attacks/SwingMeleeStats")]
+public class RangedWeapon : AttackBehavior
+{
+    public GameObject projectilePrefab;
+    public override void Activate(WeaponManager p)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Fire(Transform target, Vector2 dir)
+    {
+        GameObject proj = Instantiate(projectilePrefab, target.position, Quaternion.identity);
+        Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = dir * speed;
+        Destroy(proj, lifeTime);
+    }
+}
