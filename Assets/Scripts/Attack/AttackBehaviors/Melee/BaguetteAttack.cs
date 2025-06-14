047219558f19bb721d42bec5ab11f08fb0989247
Eric Li <15696082812@163.com>
2025-06-14 15:13:00 +0200

    .

--- Assets/Scripts/Attack/AttackBehaviors/Melee/BaguetteAttack.cs
+++ Assets/Scripts/Attack/AttackBehaviors/Melee/BaguetteAttack.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class BaguetteAttack : MonoBehaviour
+public class BaguetteAttack : SwingAttack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Attack/AttackBehaviors/Melee/SushiAttack.cs
+++ Assets/Scripts/Attack/AttackBehaviors/Melee/SushiAttack.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class SushiAttack : MonoBehaviour
+public class SushiAttack : SwingAttack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
